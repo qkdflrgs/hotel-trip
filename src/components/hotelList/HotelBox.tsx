5d9149d596cf50a52542810231319798aec0c585
qkdflrgs <qkdflrgs@gmail.com>
2024-05-28 03:37:34 +0900

    feat(hotelList): add HotelBox component

---
+++ src/components/hotelList/HotelBox.tsx
@@ -0,0 +1,51 @@
+import { Hotel } from '@models/hotel'
+import ListRow from '@shared/ListRow'
+import Flex from '@shared/Flex'
+import Text from '@shared/Text'
+import Spacing from '@shared/Spacing'
+import { css } from '@emotion/react'
+import addDelimiter from '@/utils/addDelimiter'
+
+interface HotelProps {
+  hotel: Hotel
+}
+
+function HotelBox({ hotel }: HotelProps) {
+  return (
+    <div>
+      <ListRow
+        contents={
+          <Flex direction="column">
+            <ListRow.Texts title={hotel.name} subTitle={hotel.comment} />
+            <Spacing size={4} />
+            <Text typography="t7" color="grey600">
+              {hotel.starRating}성급
+            </Text>
+          </Flex>
+        }
+        right={
+          <Flex direction="column" align="flex-end">
+            <img src={hotel.mainImageUrl} alt="호텔 이미지" css={imageStyles} />
+            <Spacing size={8} />
+            <Text bold={true}>{addDelimiter(hotel.price)}원</Text>
+          </Flex>
+        }
+        style={containerStyles}
+      />
+    </div>
+  )
+}
+
+const containerStyles = css`
+  align-items: flex-start;
+`
+
+const imageStyles = css`
+  width: 90px;
+  height: 110px;
+  border-radius: 8px;
+  object-fit: cover;
+  margin-left: 16px;
+`
+
+export default HotelBox
