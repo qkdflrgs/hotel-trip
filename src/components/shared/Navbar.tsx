391cd3a997b6bee5dc6645136f3ded7ae4eda5fb
qkdflrgs <qkdflrgs@gmail.com>
2024-05-27 18:07:59 +0900

    initial settings

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,55 @@
+import { colors } from '@styles/colorPalette'
+import { css } from '@emotion/react'
+import { Link, useLocation } from 'react-router-dom'
+import Button from './Button'
+import Flex from './Flex'
+import { useCallback } from 'react'
+
+function Navbar() {
+  const user = null
+  const location = useLocation()
+  const showSignButton =
+    ['/signup', '/signin'].includes(location.pathname) === false
+
+  const renderButton = useCallback(() => {
+    if (user != null) {
+      return (
+        <Link to="/my">
+          <img src="" alt="" />
+        </Link>
+      )
+    }
+
+    if (showSignButton) {
+      return (
+        <Link to="/signup">
+          <Button>로그인/회원가입</Button>
+        </Link>
+      )
+    }
+
+    return null
+  }, [user, showSignButton])
+
+  return (
+    <Flex
+      justify={'space-between'}
+      align={'center'}
+      css={navbarContainerStyles}
+    >
+      <Link to="/">홈</Link>
+      {renderButton()}
+    </Flex>
+  )
+}
+
+const navbarContainerStyles = css`
+  position: sticky;
+  top: 0;
+  background-color: ${colors.white};
+  z-index: 10;
+  border-bottom: 1px solid ${colors.grey};
+  padding: 10px 24px;
+`
+
+export default Navbar
