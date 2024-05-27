81585e3df3ea5a431353136eb2cd2e9484f3dc3c
qkdflrgs <qkdflrgs@gmail.com>
2024-05-28 03:39:37 +0900

    feat(hotelList): add HotelList page

--- src/pages/HotelList.tsx
+++ src/pages/HotelList.tsx
@@ -1,5 +1,40 @@
+import Top from '@shared/Top'
+import useHotels from '@components/hotelList/hooks/useHotels'
+import HotelBox from '@components/hotelList/HotelBox'
+import { Fragment } from 'react'
+import Spacing from '@shared/Spacing'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
 function HotelListPage() {
-  return <div>Hotel List</div>
+  const { data: hotels, hasNextPage, loadMore } = useHotels()
+
+  return (
+    <div>
+      <Top title="인기 호텔" subTitle="호텔부터 펜션까지 최저가" />
+      <InfiniteScroll
+        dataLength={hotels?.length ?? 0}
+        hasMore={hasNextPage}
+        loader={<></>}
+        next={loadMore}
+        scrollThreshold="100px"
+      >
+        <ul>
+          {hotels?.map((hotel, index) => (
+            <Fragment key={hotel.id}>
+              <HotelBox hotel={hotel} />
+              {hotels.length - 1 === index ? null : (
+                <Spacing
+                  size={8}
+                  backgroundColor="grey100"
+                  style={{ margin: '20px 0' }}
+                />
+              )}
+            </Fragment>
+          ))}
+        </ul>
+      </InfiniteScroll>
+    </div>
+  )
 }
 
 export default HotelListPage
