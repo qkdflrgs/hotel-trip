42bd6ff7341a14783f0fa48bfbd64dc49767e2a4
qkdflrgs <qkdflrgs@gmail.com>
2024-05-29 04:14:03 +0900

    feat(recommend): add RecommendHotelButton to Test page

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,9 +1,11 @@
+import RecommendHotelButton from '@components/test/RecommendHotelButton'
 import HotelListAddButton from '@components/test/HotelListAddButton'
 
 function TestPage() {
   return (
     <div>
       <HotelListAddButton />
+      <RecommendHotelButton />
     </div>
   )
 }
