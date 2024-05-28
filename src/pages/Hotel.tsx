a78cc4bbf2113b2967a805b12fe7b79c2541c3f1
qkdflrgs <qkdflrgs@gmail.com>
2024-05-29 00:30:43 +0900

    feat(map): add Map to Hotel page

--- src/pages/Hotel.tsx
+++ src/pages/Hotel.tsx
@@ -4,6 +4,7 @@ import Carousel from '@components/hotel/Carousel'
 import useHotel from '@components/hotel/hooks/useHotel'
 import Top from '@shared/Top'
 import { useParams } from 'react-router-dom'
+import Map from '@components/hotel/Map'
 
 function HotelPage() {
   const { id } = useParams() as { id: string }
@@ -17,6 +18,7 @@ function HotelPage() {
       <Carousel images={hotel.images} />
       <Rooms hotelId={hotel.id} />
       <Contents contents={hotel.contents} />
+      <Map location={hotel.location} />
     </div>
   )
 }
