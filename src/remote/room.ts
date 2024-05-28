ad3cffd5e43f0b285986204c5f48525a6fa1a581
qkdflrgs <qkdflrgs@gmail.com>
2024-05-29 00:03:20 +0900

    feat(hotelDetail): add getRooms(function to fetch rooms data)

---
+++ src/remote/room.ts
@@ -0,0 +1,15 @@
+import { Room } from '@models/room'
+import { COLLECTIONS } from '@constants'
+import { collection, doc, getDocs } from 'firebase/firestore'
+import { store } from './firebase'
+
+export async function getRooms(hotelId: string) {
+  const snapshot = await getDocs(
+    collection(doc(store, COLLECTIONS.HOTEL, hotelId), COLLECTIONS.ROOM),
+  )
+
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...(doc.data() as Room),
+  }))
+}
