73a1f785db961e46ce020347c013ac9da86e7b1d
qkdflrgs <qkdflrgs@gmail.com>
2024-05-29 00:02:27 +0900

    feat(hotelDetail): add Hotel page

---
+++ src/pages/Hotel.tsx
@@ -0,0 +1,24 @@
+import Contents from '@components/hotel/Contents'
+import Rooms from '@components/hotel/Rooms'
+import Carousel from '@components/hotel/Carousel'
+import useHotel from '@components/hotel/hooks/useHotel'
+import Top from '@shared/Top'
+import { useParams } from 'react-router-dom'
+
+function HotelPage() {
+  const { id } = useParams() as { id: string }
+  const { data: hotel, isLoading } = useHotel({ id })
+
+  if (hotel == null || isLoading) return <div>Loading...</div>
+
+  return (
+    <div>
+      <Top title={hotel.name} subTitle={hotel.comment} />
+      <Carousel images={hotel.images} />
+      <Rooms hotelId={hotel.id} />
+      <Contents contents={hotel.contents} />
+    </div>
+  )
+}
+
+export default HotelPage
