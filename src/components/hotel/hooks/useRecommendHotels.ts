1341975cfd2c5da7969272b44428ff752b4748bc
qkdflrgs <qkdflrgs@gmail.com>
2024-05-29 04:15:20 +0900

    feat(recommend): add useRecommendHotels hook(hook to fetch recommend hotels)

---
+++ src/components/hotel/hooks/useRecommendHotels.ts
@@ -0,0 +1,12 @@
+import { getRecommendHotels } from '@/remote/hotel'
+import { useQuery } from 'react-query'
+
+function useRecommendHotels({ hotelIds }: { hotelIds: string[] }) {
+  return useQuery({
+    queryKey: ['recommendHotels', JSON.stringify(hotelIds)],
+    queryFn: () => getRecommendHotels(hotelIds),
+    enabled: hotelIds.length > 0,
+  })
+}
+
+export default useRecommendHotels
