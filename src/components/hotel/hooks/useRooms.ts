713e55c716850534727ad63fbb61a5f5f7b3a28d
qkdflrgs <qkdflrgs@gmail.com>
2024-05-29 00:01:55 +0900

    feat(hotelDetail): add useRooms hook(hook to get rooms data)

---
+++ src/components/hotel/hooks/useRooms.ts
@@ -0,0 +1,36 @@
+import { Room } from '@models/room'
+import { COLLECTIONS } from '@constants'
+import { store } from '@remote/firebase'
+import { getRooms } from '@remote/room'
+import { collection, doc, onSnapshot } from 'firebase/firestore'
+import { useEffect } from 'react'
+import { useQuery, useQueryClient } from 'react-query'
+
+function useRooms({ hotelId }: { hotelId: string }) {
+  const client = useQueryClient()
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(doc(store, COLLECTIONS.HOTEL, hotelId), COLLECTIONS.ROOM),
+      (snapshot) => {
+        const newRooms = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...(doc.data() as Room),
+        }))
+
+        client.setQueriesData(['rooms', hotelId], newRooms)
+      },
+    )
+
+    return () => {
+      unsubscribe()
+    }
+  }, [hotelId, client])
+
+  return useQuery({
+    queryKey: ['rooms', hotelId],
+    queryFn: () => getRooms(hotelId),
+  })
+}
+
+export default useRooms
