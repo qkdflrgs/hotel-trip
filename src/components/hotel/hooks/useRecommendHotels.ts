7499b066cd9eaa0382be3a1629caca0508915193
qkdflrgs <qkdflrgs@gmail.com>
2024-05-29 20:27:56 +0900

    feat(signin): fix paths

--- src/components/hotel/hooks/useRecommendHotels.ts
+++ src/components/hotel/hooks/useRecommendHotels.ts
@@ -1,4 +1,4 @@
-import { getRecommendHotels } from '@/remote/hotel'
+import { getRecommendHotels } from '@remote/hotel'
 import { useQuery } from 'react-query'
 
 function useRecommendHotels({ hotelIds }: { hotelIds: string[] }) {
