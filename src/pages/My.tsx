759f8e766c9a8a7e60a4b67ab34367662dd5aa56
qkdflrgs <qkdflrgs@gmail.com>
2024-05-29 20:30:40 +0900

    feat(signin): add My page

---
+++ src/pages/My.tsx
@@ -0,0 +1,13 @@
+import Spacing from '@shared/Spacing'
+import Flex from '@shared/Flex'
+
+function MyPage() {
+  return (
+    <Flex>
+      <Spacing size={100} />
+      <img src="" alt="프로필 이미지" />
+    </Flex>
+  )
+}
+
+export default MyPage
