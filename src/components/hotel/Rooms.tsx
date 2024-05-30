be9bacfefe6a24686824506111c08b9d27d76e80
qkdflrgs <qkdflrgs@gmail.com>
2024-05-31 03:49:47 +0900

    feat(reservation): add reservation to Rooms component

--- src/components/hotel/Rooms.tsx
+++ src/components/hotel/Rooms.tsx
@@ -5,15 +5,22 @@ import Text from '@shared/Text'
 import ListRow from '@shared/ListRow'
 import Tag from '@shared/Tag'
 import useRooms from './hooks/useRooms'
-import Spacing from '../shared/Spacing'
+import Spacing from '@shared/Spacing'
 import { css } from '@emotion/react'
-import Button from '../shared/Button'
+import Button from '@shared/Button'
+import qs from 'qs'
+import useUser from '@hooks/auth/useUser'
+import { useAlertContext } from '@contexts/AlertContext'
+import { useNavigate } from 'react-router-dom'
 
 interface RoomsProps {
   hotelId: string
 }
 
 function Rooms({ hotelId }: RoomsProps) {
+  const user = useUser()
+  const { open } = useAlertContext()
+  const navigate = useNavigate()
   const { data: rooms } = useRooms({ hotelId })
 
   return (
@@ -28,6 +35,16 @@ function Rooms({ hotelId }: RoomsProps) {
         {rooms?.map((room) => {
           const isLast = room.availableCount === 1
           const isSoldOut = room.availableCount === 0
+
+          const params = qs.stringify(
+            {
+              roomId: room.id,
+              hotelId,
+            },
+            {
+              addQueryPrefix: true,
+            },
+          )
 
           return (
             <ListRow
@@ -58,7 +75,22 @@ function Rooms({ hotelId }: RoomsProps) {
                 />
               }
               right={
-                <Button size="medium" disabled={isSoldOut}>
+                <Button
+                  size="medium"
+                  disabled={isSoldOut}
+                  onClick={() => {
+                    if (user == null) {
+                      open({
+                        title: '로그인이 필요한 기능입니다.',
+                        onButtonClick: () => navigate('/signin'),
+                      })
+
+                      return
+                    }
+
+                    navigate(`/schedule${params}`)
+                  }}
+                >
                   {isSoldOut ? '매진' : '선택'}
                 </Button>
               }
