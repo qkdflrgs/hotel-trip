c1348f028601ca5482cfab998ef8f57df71fe027
qkdflrgs <qkdflrgs@gmail.com>
2024-05-31 03:56:56 +0900

    feat(reservation): add styles to Test page

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,11 +1,20 @@
 import RecommendHotelButton from '@components/test/RecommendHotelButton'
 import HotelListAddButton from '@components/test/HotelListAddButton'
+import HotelFormAddButton from '@components/test/HotelFormAddButton'
 
 function TestPage() {
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '8px',
+        padding: '24px',
+      }}
+    >
       <HotelListAddButton />
       <RecommendHotelButton />
+      <HotelFormAddButton />
     </div>
   )
 }
