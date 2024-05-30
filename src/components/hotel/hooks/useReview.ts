0c4ca5c101a2fb13d3d3e0ab880efc91f47f4882
qkdflrgs <qkdflrgs@gmail.com>
2024-05-30 21:16:20 +0900

    feat(review): add write mutation to useReview hook

--- src/components/hotel/hooks/useReview.ts
+++ src/components/hotel/hooks/useReview.ts
@@ -1,13 +1,37 @@
-import { getReviews } from '@remote/review'
-import { useQuery } from 'react-query'
+import useUser from '@hooks/auth/useUser'
+import { getReviews, writeReview } from '@remote/review'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
 
 function useReview({ hotelId }: { hotelId: string }) {
+  const user = useUser()
+  const client = useQueryClient()
+
   const { data, isLoading } = useQuery({
     queryKey: ['reviews', hotelId],
     queryFn: () => getReviews({ hotelId }),
   })
 
-  return { data, isLoading }
+  const { mutateAsync: write } = useMutation(
+    async (text: string) => {
+      const newReview = {
+        createdAt: new Date(),
+        hotelId,
+        userId: user?.uid as string,
+        text,
+      }
+
+      await writeReview(newReview)
+
+      return true
+    },
+    {
+      onSuccess: () => {
+        client.invalidateQueries(['reviews', hotelId])
+      },
+    },
+  )
+
+  return { data, isLoading, write }
 }
 
 export default useReview
