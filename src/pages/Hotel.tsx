6933c92ec608fa3423f82358705d91a1cfe7eb8a
qkdflrgs <qkdflrgs@gmail.com>
2024-05-31 23:36:45 +0900

    add ScrollProgressBar to Hotel page

--- src/pages/Hotel.tsx
+++ src/pages/Hotel.tsx
@@ -8,6 +8,8 @@ import Map from '@components/hotel/Map'
 import RecommendHotels from '@components/hotel/RecommendHotels'
 import ActionButtons from '@components/hotel/ActionButtons'
 import Review from '@components/hotel/Review'
+import ScrollProgressBar from '@shared/ScrollProgessBar'
+import { css } from '@emotion/react'
 
 function HotelPage() {
   const { id } = useParams() as { id: string }
@@ -17,6 +19,7 @@ function HotelPage() {
 
   return (
     <div>
+      <ScrollProgressBar style={scrollProgressBarStyles} />
       <Top title={hotel.name} subTitle={hotel.comment} />
       <Carousel images={hotel.images} />
       <ActionButtons hotel={hotel} />
@@ -28,5 +31,11 @@ function HotelPage() {
     </div>
   )
 }
+
+const scrollProgressBarStyles = css`
+  position: sticky;
+  top: 52.5px;
+  z-index: 2;
+`
 
 export default HotelPage
